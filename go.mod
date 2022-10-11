31d75bf3848211c5dfb349b7890dda77bfd09f46
Hasit Mistry <hasit@fluxninja.com>
2022-10-11 08:32:02 -0700

    Add `shutdown` method to `Client`
    - Update example
    - Update go.mod

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	go.buf.build/grpc/go/fluxninja/aperture v1.4.10
 	go.opentelemetry.io/otel v1.10.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.10.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.10.0
 	go.opentelemetry.io/otel/sdk v1.10.0
 	go.opentelemetry.io/otel/trace v1.10.0
@@ -21,7 +22,6 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.10.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.10.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
 	go.uber.org/goleak v1.2.0 // indirect
 	golang.org/x/net v0.0.0-20220927171203-f486391704dc // indirect
--- sdk/client.go
+++ sdk/client.go
@@ -8,11 +8,12 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
-	oteltrace "go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 
 	flowcontrolgrpc "go.buf.build/grpc/go/fluxninja/aperture/aperture/flowcontrol/v1"
@@ -21,12 +22,14 @@ import (
 // Client is the interface that is provided to the user upon which they can perform Check calls for their service and eventually shut down in case of error.
 type Client interface {
 	StartFlow(ctx context.Context, feature string, labels map[string]string) (Flow, error)
+	Shutdown(ctx context.Context) error
 }
 
 type apertureClient struct {
 	flowControlClient flowcontrolgrpc.FlowControlServiceClient
-	tracer            oteltrace.Tracer
+	tracer            trace.Tracer
 	timeout           time.Duration
+	exporter          *otlptrace.Exporter
 }
 
 // Options that the user can pass to Aperture in order to receive a new Client.
@@ -38,43 +41,43 @@ type Options struct {
 
 // NewClient returns a new Client that can be used to perform Check calls.
 // The user will pass in options which will be used to create a connection with otel and a tracerProvider retrieved from such connection.
-func NewClient(options Options) (Client, error) {
+func NewClient(ctx context.Context, opts Options) (Client, error) {
 	exporter, err := otlptracegrpc.New(
-		context.Background(),
-		otlptracegrpc.WithGRPCConn(options.ApertureAgentGRPCClientConn),
-		otlptracegrpc.WithReconnectionPeriod(defaultGRPCReconnectionTime),
+		ctx,
+		otlptracegrpc.WithGRPCConn(opts.ApertureAgentGRPCClientConn),
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	r, err := newResource()
+	res, err := newResource()
 	if err != nil {
 		return nil, err
 	}
 
-	tracerProvider := trace.NewTracerProvider(
-		trace.WithSpanProcessor(trace.NewBatchSpanProcessor(exporter)),
-		trace.WithResource(r),
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(sdktrace.NewBatchSpanProcessor(exporter)),
 	)
 
 	otel.SetTracerProvider(tracerProvider)
 
 	tracer := tracerProvider.Tracer(libraryName)
 
-	fcClient := flowcontrolgrpc.NewFlowControlServiceClient(options.ApertureAgentGRPCClientConn)
+	fcClient := flowcontrolgrpc.NewFlowControlServiceClient(opts.ApertureAgentGRPCClientConn)
 
 	var timeout time.Duration
-	if options.CheckTimeout == 0 {
+	if opts.CheckTimeout == 0 {
 		timeout = defaultRPCTimeout
 	} else {
-		timeout = options.CheckTimeout
+		timeout = opts.CheckTimeout
 	}
 
 	c := &apertureClient{
 		flowControlClient: fcClient,
 		tracer:            tracer,
 		timeout:           timeout,
+		exporter:          exporter,
 	}
 	return c, nil
 }
@@ -84,13 +87,13 @@ func NewClient(options Options) (Client, error) {
 // The call returns immediately in case connection with Aperture Agent is not established.
 // The default semantics are fail-to-wire. If StartFlow fails, calling Flow.Accepted() on returned Flow returns as true.
 func (c *apertureClient) StartFlow(ctx context.Context, feature string, explicitLabels map[string]string) (Flow, error) {
-	context, cancel := context.WithTimeout(ctx, c.timeout)
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
 	defer cancel()
 
 	labels := make(map[string]string)
 
 	// Inherit labels from baggage
-	baggageCtx := baggage.FromContext(context)
+	baggageCtx := baggage.FromContext(ctx)
 	for _, member := range baggageCtx.Members() {
 		value, err := url.QueryUnescape(member.Value())
 		if err != nil {
@@ -109,29 +112,31 @@ func (c *apertureClient) StartFlow(ctx context.Context, feature string, explicit
 		Labels:  labels,
 	}
 
-	_, span := c.tracer.Start(context, "Aperture Check")
-	span.SetAttributes(
+	_, span := c.tracer.Start(ctx, "Aperture Check", trace.WithAttributes(
 		attribute.Int64(flowStartTimestampLabel, time.Now().UnixNano()),
 		attribute.String(sourceLabel, "sdk"),
-	)
+	))
 
-	res, err := c.flowControlClient.Check(context, req)
+	f := &flow{
+		span: span,
+	}
+
+	res, err := c.flowControlClient.Check(ctx, req)
+	if err != nil {
+		f.checkResponse = nil
+	}
 
 	span.SetAttributes(
 		attribute.Int64(checkResponseTimestampLabel, time.Now().UnixNano()),
 	)
 
-	if err != nil {
-		return &flow{
-			checkResponse: nil,
-			span:          span,
-		}, err
-	}
+	f.checkResponse = res
+	return f, nil
+}
 
-	return &flow{
-		checkResponse: res,
-		span:          span,
-	}, nil
+// Shutdown shuts down the apeture client.
+func (c *apertureClient) Shutdown(ctx context.Context) error {
+	return c.exporter.Shutdown(ctx)
 }
 
 // newResource returns a resource describing the running process, containing the library name and version.
